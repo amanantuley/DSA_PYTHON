6790e02d6f3dd5ec7b0843c13666350109c4fd50
Aman Antuley <160766309+amanantuley@users.noreply.github.com>
2025-07-22 13:57:19 +0530

    Add files via upload

---
+++ Height_Checker.py
@@ -0,0 +1,13 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+
+        expected = list(heights)
+
+        expected.sort()
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count = count + 1
+
+        return count
+        
